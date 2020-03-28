3608c49c08c830e4c35d04fe8d7333e828940c6b
XiongKezhi <398682946@qq.com>
2020-03-28 13:49:55 +0800

    Using installaion event and Branch Source SCM done!

--- src/main/java/io/jenkins/plugins/CheckGHEventSubscriber.java
+++ src/main/java/io/jenkins/plugins/CheckGHEventSubscriber.java
@@ -1,11 +1,16 @@
 package io.jenkins.plugins;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nullable;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.kohsuke.github.GHEvent;
 import org.jenkinsci.plugins.github.extension.GHEventsSubscriber;
@@ -20,15 +25,10 @@ public class CheckGHEventSubscriber extends GHEventsSubscriber {
 
     private static final Logger LOGGER = Logger.getLogger(CheckGHEventSubscriber.class.getName());
 
-    private List<GHSubscriberEvent> checkSuiteEvents = new LinkedList<>();
-    private List<GHSubscriberEvent> checkRunEvents = new LinkedList<>();
+    private Map<String, Long> repoFullNameToInstallationId = new HashMap<>();
 
-    public List<GHSubscriberEvent> getCheckSuiteEvents() {
-        return checkSuiteEvents;
-    }
-
-    public List<GHSubscriberEvent> getCheckRunEvents() {
-        return checkRunEvents;
+    public long findInstallationIdByRepository(String fullName) {
+        return repoFullNameToInstallationId.getOrDefault(fullName, (long)-1);
     }
 
     public static CheckGHEventSubscriber getInstance() {
@@ -50,7 +50,7 @@ public class CheckGHEventSubscriber extends GHEventsSubscriber {
      */
     @Override
     protected Set<GHEvent> events() {
-        return immutableEnumSet(GHEvent.CHECK_RUN, GHEvent.CHECK_SUITE);
+        return immutableEnumSet(GHEvent.INSTALLATION_REPOSITORIES, GHEvent.CHECK_RUN, GHEvent.CHECK_SUITE);
     }
 
     /**
@@ -59,16 +59,41 @@ public class CheckGHEventSubscriber extends GHEventsSubscriber {
     @Override
     protected void onEvent(GHSubscriberEvent event) {
         switch (event.getGHEvent()) {
-            case CHECK_SUITE:
-                LOGGER.log(Level.FINE, "Received Check Run Event...");
-                checkSuiteEvents.add(event);
-                break;
-            case CHECK_RUN:
-                LOGGER.log(Level.FINE, "Received Check Suite Event...");
-                checkRunEvents.add(event);
+            case INSTALLATION_REPOSITORIES:
+                LOGGER.log(Level.FINE, "Received Installation Repositories Event...");
+                updateInstallations(event);
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + event);
         }
+    }
+
+    /**
+     * Update installations
+     *
+     * @param event installation repositories event
+     */
+    private void updateInstallations(GHSubscriberEvent event) {
+        JsonNode payload = null;
+        try {
+            payload = new ObjectMapper().readTree(event.getPayload());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        if (payload == null)
+            return;
+
+        long installationId = payload.get("installation").get("id").asLong();
+
+        // iterate the added repositories
+        Iterator<JsonNode> repoIter = payload.get("repositories_added").iterator();
+        while (repoIter.hasNext())
+            repoFullNameToInstallationId.put(repoIter.next().get("full_name").asText(), installationId);
+
+        // iterate the removed repositories
+        repoIter = payload.get("repositories_removed").iterator();
+        while (repoIter.hasNext())
+            repoFullNameToInstallationId.remove(repoIter.next().get("full_name").asText());
     }
 }
