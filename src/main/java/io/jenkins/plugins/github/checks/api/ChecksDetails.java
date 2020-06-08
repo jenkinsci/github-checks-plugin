058e117e01f47e65a715e4c5b0852bd900b4034b
Kezhi Xiong <august.xkz@gmail.com>
2020-06-08 21:17:38 +0800

    More details in JavaDoc for ChecksDetails

--- src/main/java/io/jenkins/plugins/github/checks/api/ChecksDetails.java
+++ src/main/java/io/jenkins/plugins/github/checks/api/ChecksDetails.java
@@ -6,6 +6,8 @@ import java.util.Objects;
 
 import org.apache.commons.lang3.StringUtils;
 
+import edu.umd.cs.findbugs.annotations.Nullable;
+
 import io.jenkins.plugins.github.checks.ChecksConclusion;
 import io.jenkins.plugins.github.checks.ChecksStatus;
 
@@ -46,10 +48,11 @@ public class ChecksDetails {
     }
 
     /**
-     * Returns the url of a site with full details of the check.
+     * Returns the url of a site with full details of a check.
      *
-     * @return the string representing the url of a site
+     * @return the url of a site or null
      */
+    @Nullable
     public String getDetailsURL() {
         return detailsURL;
     }
@@ -57,10 +60,9 @@ public class ChecksDetails {
     /**
      * Returns the conclusion of a check.
      *
-     * @return {@link ChecksConclusion}, one of {@code SUCCESS}, {@code FAILURE}, {@code NEUTRAL}, {@code CANCELLED},
-     *         {@code SKIPPED}, {@code TIME_OUT}, or {@code ACTION_REQUIRED} when {@link ChecksDetails#getStatus()}
-     *         returns {@code COMPLETED}, otherwise an empty string
+     * @return the conclusion of a check or null
      */
+    @Nullable
     public ChecksConclusion getConclusion() {
         return conclusion;
     }
@@ -68,8 +70,9 @@ public class ChecksDetails {
     /**
      * Returns the {@link Output}s of a check
      *
-     * @return An immutable list of {@link Output}s of a check
+     * @return An immutable list of {@link Output}s of a check or null
      */
+    @Nullable
     public List<Output> getOutputs() {
         return outputs;
     }
@@ -77,7 +80,7 @@ public class ChecksDetails {
     /**
      * Returns the {@link Action}s of a check
      *
-     * @return An immutable list of {@link Action}s of a check
+     * @return An immutable list of {@link Action}s of a check or null
      */
     public List<Action> getActions() {
         return actions;
@@ -91,8 +94,22 @@ public class ChecksDetails {
         private List<Output> outputs = Collections.emptyList();
         private List<Action> actions = Collections.emptyList();
 
-        public ChecksDetailsBuilder(final String name, final ChecksStatus status)
-                throws IllegalArgumentException{
+        /**
+         * Construct a builder with the given name and status.
+         *
+         * <p>
+         *     The name will be the same as the check run's name shown on GitHub UI and GitHub uses this name to
+         *     identify a check run, so make sure this name is unique, e.g. "code-coverage".
+         * <p>
+         *
+         * @param name
+         *         the name of the check run
+         * @param status
+         *         the status which the check run will be set
+         *
+         * @throws IllegalArgumentException if the name is blank or the status is null
+         */
+        public ChecksDetailsBuilder(final String name, final ChecksStatus status) {
             Objects.requireNonNull(status);
             if (StringUtils.isBlank(name)) {
                 throw new IllegalArgumentException("check name should not be blank");
@@ -102,12 +119,39 @@ public class ChecksDetails {
             this.status = status;
         }
 
+        /**
+         * Set the url of a site with full details of a check.
+         *
+         * <p>
+         *     If the details url is not set, the Jenkins build url will be used,
+         *     e.g. ci.jenkins.io/job/Core/job/jenkins/job/master/2000/.
+         * </p>
+         *
+         * @param detailsURL
+         *         the url of the site which shows the detail information of the check
+         * @return this builder
+         * @throws NullPointerException if the <code>detailsURL</code> is null
+         */
         public ChecksDetailsBuilder withDetailsURL(final String detailsURL) {
             Objects.requireNonNull(detailsURL);
             this.detailsURL = detailsURL;
             return this;
         }
 
+        /**
+         * Set the conclusion of a check.
+         *
+         * <p>
+         *     The conclusion should only be set when the <code>status</code> is set {@link ChecksStatus#COMPLETED} when
+         *     constructing this builder.
+         * </p>
+         *
+         * @param conclusion
+         *         the conclusion
+         * @return this builder
+         * @throws NullPointerException if the <code>conclusion</code> is null
+         * @throws IllegalArgumentException if the <code>status</code> is not {@link ChecksStatus#COMPLETED}
+         */
         public ChecksDetailsBuilder withConclusion(final ChecksConclusion conclusion) {
             Objects.requireNonNull(conclusion);
 
@@ -118,12 +162,28 @@ public class ChecksDetails {
             return this;
         }
 
+        /**
+         * Set the outputs of a check.
+         *
+         * @param outputs
+         *         a list of outputs
+         * @return this builder
+         * @throws NullPointerException if the <code>outputs</code> is null
+         */
         public ChecksDetailsBuilder withOutputs(final List<Output> outputs) {
             Objects.requireNonNull(outputs);
             this.outputs = Collections.unmodifiableList(outputs);
             return this;
         }
 
+        /**
+         * Set the actions of a check.
+         *
+         * @param actions
+         *         a list of actions
+         * @return this builder
+         * @throws NullPointerException if the <code>actions</code> is null
+         */
         public ChecksDetailsBuilder withActions(List<Action> actions) {
             Objects.requireNonNull(actions);
             this.actions = Collections.unmodifiableList(actions);
@@ -136,7 +196,7 @@ public class ChecksDetails {
          * @return the built {@code ChecksDetail}
          * @throws IllegalArgumentException if {@code conclusion} is null when {@code status} is {@code completed}
          */
-        public ChecksDetails build() throws IllegalArgumentException {
+        public ChecksDetails build() {
             if (conclusion == null && status == ChecksStatus.COMPLETED) {
                 throw new IllegalArgumentException("conclusion must be set when status is completed");
             }
