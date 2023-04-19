4e4c250d4a5ae0da9fb874bbdfd92ed6ce8d9518
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-04-19 17:09:22 +0100

    Re-enable integration tests (#332)

---
+++ src/test/java/io/jenkins/plugins/checks/github/GitSCMChecksContextITest.java
@@ -0,0 +1,88 @@
+package io.jenkins.plugins.checks.github;
+
+import hudson.model.Action;
+import hudson.model.Result;
+import java.util.Collections;
+
+import jenkins.model.ParameterizedJobMixIn;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import hudson.model.FreeStyleProject;
+import hudson.model.Run;
+import hudson.plugins.git.BranchSpec;
+import hudson.plugins.git.GitSCM;
+import org.jvnet.hudson.test.JenkinsRule;
+
+/**
+ * Integration tests for {@link GitSCMChecksContext}.
+ */
+public class GitSCMChecksContextITest {
+    private static final String EXISTING_HASH = "4ecc8623b06d99d5f029b66927438554fdd6a467";
+    private static final String HTTP_URL = "https://github.com/jenkinsci/github-checks-plugin.git";
+    private static final String CREDENTIALS_ID = "credentials";
+    private static final String URL_NAME = "url";
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    /**
+     * Creates a FreeStyle job that uses {@link hudson.plugins.git.GitSCM} and runs a successful build.
+     * Then this build is used to create a new {@link GitSCMChecksContext}. So the build actually is not publishing
+     * the checks we just ensure that we can create the context with the successful build (otherwise we would need
+     * Wiremock to handle the requests to GitHub).
+     */
+    @Test
+    public void shouldRetrieveContextFromFreeStyleBuild() throws Exception {
+        FreeStyleProject job = j.createFreeStyleProject();
+        
+        BranchSpec branchSpec = new BranchSpec(EXISTING_HASH);
+        GitSCM scm = new GitSCM(GitSCM.createRepoList(HTTP_URL, CREDENTIALS_ID),
+                Collections.singletonList(branchSpec), false, Collections.emptyList(), 
+                null, null, Collections.emptyList());
+        job.setScm(scm);
+
+        Run<?, ?> run = buildSuccessfully(job);
+
+        GitSCMChecksContext gitSCMChecksContext = new GitSCMChecksContext(run, URL_NAME);
+
+        assertThat(gitSCMChecksContext.getRepository()).isEqualTo("jenkinsci/github-checks-plugin");
+        assertThat(gitSCMChecksContext.getHeadSha()).isEqualTo(EXISTING_HASH);
+        assertThat(gitSCMChecksContext.getCredentialsId()).isEqualTo(CREDENTIALS_ID);
+    }
+
+    private Run<?, ?> buildSuccessfully(ParameterizedJobMixIn.ParameterizedJob<?, ?> job) throws Exception {
+        return j.assertBuildStatus(Result.SUCCESS, job.scheduleBuild2(0, new Action[0]));
+    }
+
+    /**
+     * Creates a pipeline that uses {@link hudson.plugins.git.GitSCM} and runs a successful build.
+     * Then this build is used to create a new {@link GitSCMChecksContext}. 
+     */
+    @Test 
+    public void shouldRetrieveContextFromPipeline() throws Exception {
+        WorkflowJob job = j.createProject(WorkflowJob.class);
+        
+        job.setDefinition(new CpsFlowDefinition("node {\n" 
+                + "  stage ('Checkout') {\n" 
+                + "    checkout scm: ([\n"
+                + "                    $class: 'GitSCM',\n"
+                + "                    userRemoteConfigs: [[credentialsId: '" + CREDENTIALS_ID + "', url: '" + HTTP_URL + "']],\n"
+                + "                    branches: [[name: '" + EXISTING_HASH + "']]\n"
+                + "            ])"
+                + "  }\n" 
+                + "}\n", true));
+        
+        Run<?, ?> run = buildSuccessfully(job);
+
+        GitSCMChecksContext gitSCMChecksContext = new GitSCMChecksContext(run, URL_NAME);
+
+        assertThat(gitSCMChecksContext.getRepository()).isEqualTo("jenkinsci/github-checks-plugin");
+        assertThat(gitSCMChecksContext.getCredentialsId()).isEqualTo(CREDENTIALS_ID);
+        assertThat(gitSCMChecksContext.getHeadSha()).isEqualTo(EXISTING_HASH); 
+    }
+}
---
+++ src/test/java/io/jenkins/plugins/checks/github/config/GitHubChecksConfigITest.java
@@ -0,0 +1,33 @@
+package io.jenkins.plugins.checks.github.config;
+
+import hudson.util.StreamTaskListener;
+import io.jenkins.plugins.checks.github.GitHubChecksPublisherFactory;
+import java.io.IOException;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Integration test for {@link GitHubChecksConfig}.
+ */
+public class GitHubChecksConfigITest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    /**
+     * When a job has not {@link org.jenkinsci.plugins.github_branch_source.GitHubSCMSource} or
+     * {@link hudson.plugins.git.GitSCM}, the default config should be used and no verbose log should be output.
+     */
+    @Test
+    public void shouldUseDefaultConfigWhenNoSCM() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        GitHubChecksPublisherFactory.fromJob(j.createFreeStyleProject(), new StreamTaskListener(os));
+
+        assertThat(os.toString()).doesNotContain("Causes for no suitable publisher found: ");
+    }
+}
